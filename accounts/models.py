3d9ac5076b4d2bc5684ad6c25b46979e2987548d
Mani Zamani <mani.84.zmn@gmail.com>
2025-09-27 14:11:18 -0700

    feat: inital user Entity

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,17 @@
 from django.contrib.auth.models import AbstractUser
-
+from django.db import models
 
 class User(AbstractUser):
-    pass
+    address = models.TextField(blank=True , null=True)
+    age = models.PositiveSmallIntegerField(blank=True , null=True)
+    description = models.TextField(blank=True , null=True)
+    gender = models.CharField(
+        max_length=1,
+        choices=[
+        ("M","Male"),
+        ("F","Female")]
+    ,blank=True , null=True)
+    phone = models.CharField(max_length=15 , blank=True , null=True)
+
+    def __str__(self):
+        return self.username
