c783fe4b5209925dabcb20e2f7c7ee7328d439e0
Mani Zamani <mani.84.zmn@gmail.com>
2025-09-27 14:18:31 -0700

    feat: initial Entity Benefactor

--- charities/models.py
+++ charities/models.py
@@ -1,7 +1,14 @@
 from django.db import models
-
+from accounts.models import User
 
 class Benefactor(models.Model):
+    user = models.OneToOneField(User , on_delete=models.CASCADE)
+    experience = models.SmallIntegerField(choices=[
+        (0,'Intremediate'),
+        (1, 'Midlevel'),
+        (2, 'Pro')
+    ],default=0)
+    free_time_per_week = models.PositiveSmallIntegerField(default=0)
     pass
 
 
