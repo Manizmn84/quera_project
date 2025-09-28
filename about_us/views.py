1fb3bf20337b6cbbdf4d5d8df97ad6823a380b60
Mani Zamani <mani.84.zmn@gmail.com>
2025-09-28 05:36:31 -0700

    feat: create the view for show the list of users

--- about_us/views.py
+++ about_us/views.py
@@ -1,3 +1,9 @@
+from django.shortcuts import render
+from accounts.models import User
+
 def about_us(request):
     # View for about-us page
-    ...
+    users = User.objects.all()
+    render(request , "about_us.html" , {
+        "users" : users,
+    })
